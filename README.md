54ea12a569c785a387642ca43b91c7ef0584ad41
Lotfi <hl_feraga@esi.dz>
2021-07-30 23:08:21 +0100

    training the model we created :')

--- README.md
+++ README.md
@@ -24,4 +24,17 @@ The dataset contains more than 50,000 images of different traffic signs. It is f
 1.Explore the dataset        
 2.Build a CNN model          
 3.Train and validate the model        
-4.Test the model with test dataset      
+4.Test the model with test dataset    
+
+# Architecture of the built model 
+2 Conv2D layer (filter=32, kernel_size=(5,5), activation=”relu”)   
+MaxPool2D layer ( pool_size=(2,2))    
+Dropout layer (rate=0.25)        
+2 Conv2D layer (filter=64, kernel_size=(3,3), activation=”relu”)       
+MaxPool2D layer ( pool_size=(2,2))       
+Dropout layer (rate=0.25)     
+Flatten layer to squeeze the layers into 1 dimension     
+Dense Fully connected layer (256 nodes, activation=”relu”)     
+Dropout layer (rate=0.5)     
+Dense layer (43 nodes, activation=”softmax”)  
+
--- traffic_signs_recognition.py
+++ traffic_signs_recognition.py
@@ -8,7 +8,8 @@ import os
 from sklearn.model_selection import train_test_split 
 from tensorflow.keras.utils import to_categorical 
 from tensorflow.keras.models import Sequential
-from keras.layers import Conv2D , MaxPool2D ,Dense , Flatten , Dropout 
+from keras.layers import Conv2D , MaxPool2D ,Dense , Flatten , Dropout
+from tensorflow.python.keras.backend import dropout 
 
 data = []
 labels = []
@@ -32,3 +33,35 @@ for x in range(classes) :
 
 data = np.array(data)
 labels = np.array(labels)
+
+print(data.shape , labels.shape)
+
+x_train,x_test , y_train , y_test = train_test_split(data , labels , test_size = 0.2 , random_state=42)
+
+print(x_train.shape,x_test.shape , y_train.shape , y_test.shape )
+
+y_train = to_categorical(y_train , 43)
+y_test = to_categorical(y_test , 43)
+
+#builing the CNN model 
+
+model = Sequential()
+model.add(Conv2D(filters=32,kernel_size=(5,5),activation='relu',input_shape= x_train.shape[1:]))
+model.add(Conv2D(filters=32 ,kernel_size=(5,5), activation='relu'))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters = 64, kernel_size=(3,3),activation='relu'))
+model.add(Conv2D(filters = 64, kernel_size=(3,3),activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256 , activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+# compiling the model 
+model.compile(loss='categorical_crossentropy', optimizer='adam' ,metrics=['accuracy'])
+
+#train and validate the model 
+
+history = model.fit(x_train, y_train , batch_size = 64, epochs = 15 , validation_data=(x_test, y_test))
+
